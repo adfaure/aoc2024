42830a7ec0eaa10c7fd7bb53efddaab8c77f01be
Adrien Faure <adrien.faure@protonmail.com>
2024-11-22 22:27:02 +0100

    prepare for 2024

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay"; # use master since newer Rust versions are continuously pushed
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in {
+      defaultApp = utils.lib.mkApp {
+        drv = self.defaultPackage."${system}";
+      };
+
+      devShell = with pkgs;
+        mkShell rec {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          PKG_CONFIG_ALLOW_SYSTEM_CFLAGS = "1";
+        };
+
+      formatter = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    });
+}
