7d557d0fcf436ae154b0dc3641f706d4c942e4b2
Adrien Faure <adrien.faure@protonmail.com>
2024-12-16 23:47:20 +0100

    day-16: ok

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,20 +1,28 @@
 #![feature(iter_array_chunks)]
 use itertools::Itertools;
+use std::boxed::Box;
 use std::cmp::Ordering;
+use std::collections::VecDeque;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 use std::io::{BufRead, BufReader};
-use std::iter::once;
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+#[derive(Clone, Eq, PartialEq, Debug)]
 struct State {
     cost: usize,
     position: (i32, i32),
     dir: (i32, i32),
+    prev: Option<Box<State>>,
 }
-struct Edge {
-    node: usize,
-    cost: usize,
+
+impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.cost.hash(state);
+        self.position.hash(state);
+        self.dir.hash(state);
+        // Skip hashing `prev`.
+    }
 }
 
 impl Ord for State {
@@ -26,6 +34,7 @@ impl Ord for State {
             .cost
             .cmp(&self.cost)
             .then_with(|| self.position.cmp(&other.position))
+            .then_with(|| self.dir.cmp(&other.dir))
     }
 }
 
@@ -79,188 +88,249 @@ fn main() -> std::io::Result<()> {
         .unwrap();
 
     // Should print the p1 on its own
-    shortest_path_p1(&grid, start, end);
+    let min = shortest_path_p1(&grid, start, end, (1, 0));
 
-    let mut lol = shortest_path_p2(&grid, start, 0, (1, 0), end, HashSet::new());
+    let best = shortest_path_p2_recurse(
+        &grid,
+        start,
+        (1, 0),
+        0,
+        end,
+        HashSet::new(),
+        HashSet::new(),
+    );
+    // show_grid(&grid, &best.clone().unwrap().1);
 
-    lol.sort_by(|a, b| a.0.cmp(&b.0));
-
-    for (cost, path) in lol.iter().take(4) {
-        println!("cost: {cost}");
-        show_grid(&grid, path);
-    }
-
-    let min = lol[0].0;
-
-    println!("min: {}", min);
-    let best_places = lol
-        .into_iter()
-        .take_while(|e| e.0 == min)
-        .flat_map(|(cost, path)| path)
-        .collect::<HashSet<_>>();
-
-    show_grid(&grid, &best_places);
-
+    println!("p2: {}", best.unwrap().1.len());
     Ok(())
 }
 
-fn shortest_path_p2(
+fn shortest_path_p2_recurse(
     grid: &[Vec<char>],
     start: (i32, i32),
+    start_dir: (i32, i32),
     start_cost: usize,
-    init_dir: (i32, i32),
     goal: (i32, i32),
+    path_to: HashSet<(i32, i32)>,
     mut seen: HashSet<((i32, i32), (i32, i32))>,
-) -> Vec<(i32, HashSet<(i32, i32)>)> {
-    let mut heap = BinaryHeap::new();
+) -> Option<(i32, HashSet<(i32, i32)>)> {
+    // println!("Starting with: {start:?} {start_dir:?} {goal:?}, {start_cost}");
 
-    // to retrieve the path
-    let mut paths: HashMap<(i32, i32), Option<State>> = HashMap::new();
+    let mut heap = BinaryHeap::new();
+    let mut results = HashSet::new();
 
     let dims = (grid[0].len() as i32, grid.len() as i32);
 
     let initial_state = State {
         cost: start_cost,
         position: start,
-        dir: init_dir,
+        prev: None,
+        dir: start_dir,
     };
-
-    paths.insert(start, None);
-
-
-    // println!("At: {:?} dir: {:?}", start, init_dir);
 
     heap.push(initial_state);
 
-    let mut best_position = HashSet::new();
-    let mut answers = vec![];
+    let min = shortest_path_p1(grid, start, goal, start_dir);
 
     while let Some(state) = heap.pop() {
-        let cost = state.cost;
-        let current_dir = state.dir;
-        let position = state.position;
+        let State {
+            cost,
+            position,
+            dir,
+            ref prev,
+        } = state;
 
-        if position == goal {
-            // println!("found goal from: {start:?} {current_dir:?}");
-            // That means that we reach the goal
-            // We are likely to be in a subpath
-            let mut traceback = position;
-            let mut path = vec![];
-
-            while let Some(Some(prev)) = paths.get(&traceback) {
-                path.push(prev);
-                traceback = prev.position;
-                best_position.insert(prev.position);
-            }
-
-            answers.push(
-                (cost as i32,
-                HashSet::from_iter(path.iter().map(|s| s.position)))
-            );
+        if cost > min + start_cost {
+            return None;
         }
 
-        if seen.insert((position, current_dir)) {
+        let mut path: Vec<State> = vec![state.clone()];
+        let mut cur: Option<Box<State>> = state.prev.clone();
+        let mut current_best_pos = HashSet::new();
+
+        while let Some(prev) = cur {
+            path.push(*prev.clone());
+            cur = prev.prev;
+        }
+
+        current_best_pos.extend(path_to.clone().iter());
+        current_best_pos.extend(path.iter().map(|state| state.position));
+
+        if position == goal {
+            // println!("found");
+            results.extend(path_to.clone().iter());
+            results.extend(path.iter().map(|state| state.position));
+
+            return Some((cost as i32, results));
+        }
+
+        if seen.insert((position, dir)) {
             let mut todos = vec![];
 
-            for vec in [current_dir, (current_dir.1, -current_dir.0), (-current_dir.1, current_dir.0)] {
+            for vec in [dir, (dir.1, -dir.0), (-dir.1, dir.0)] {
                 let new_pos = (position.0 + vec.0, position.1 + vec.1);
                 if new_pos.0 >= 0 && new_pos.0 < dims.0 && new_pos.1 >= 0 && new_pos.1 < dims.1 {
                     let cell = grid[new_pos.1 as usize][new_pos.0 as usize];
                     if cell == '#' {
                         continue;
                     } else {
-                        let score = if vec == current_dir {
+                        let score = if vec == dir {
                             cost + 1
                         } else {
-                            cost + 1000 + 1
+                            cost + 1000 + 1 //FIXME: not sure about this +1
                         };
 
                         let next = State {
                             cost: score,
                             position: new_pos,
                             dir: vec,
+                            prev: Some(Box::new(state.clone())),
                         };
 
-                        match paths.get(&new_pos) {
-                            Some(Some(current_state)) => {
-                                if score < current_state.cost {
-                                    paths.insert(new_pos, Some(state));
-                                }
-                            }
-                            Some(_) => {
-                                // unreachable!()
-                            }
-                            None => {
-                                paths.insert(new_pos, Some(state));
-                            }
-                        };
-
-                        todos.push(next);
+                        // fifo.push_front(next);
+                        if !path_to.contains(&next.position) {
+                            todos.push(next);
+                        }
                     }
-                }
-                // println!("todos: {todos:?}");
-
-                #[allow(clippy::comparison_chain)]
-                if todos.len() > 1 {
-                    // We are likely to be in a subpath
-                    let mut traceback = position;
-                    let mut path = vec![];
-                    path.push(&state);
-
-                    while let Some(Some(prev)) = paths.get(&traceback) {
-                        path.push(prev);
-                        traceback = prev.position;
-                        best_position.insert(prev.position);
-                    }
-
-                    let path_set: HashSet<(i32, i32)> =
-                        HashSet::from_iter(path.iter().map(|s| s.position));
-
-                    let mut all_subpaths = vec![];
-
-                    for todo in &todos {
-                        let results = shortest_path_p2(
-                            grid,
-                            todo.position,
-                            todo.cost,
-                            todo.dir,
-                            goal,
-                            seen.clone(),
-                        )
-                        .into_iter()
-                        .filter(|e| e.0 > 0)
-                        .map(|(cost, sub_path)| {
-                            (
-                                cost,
-                                HashSet::from_iter(
-                                    path_set
-                                        .clone()
-                                        .into_iter()
-                                        .chain(sub_path.clone().into_iter()),
-                                ),
-                            )
-                        });
-
-                        all_subpaths.extend(results);
-                    }
-                    answers.extend(all_subpaths);
-                } else if todos.len() == 1 {
-                    heap.push(todos[0])
                 }
             }
-        } else {
-            // println!("Already seen :{state:?}");
+
+            #[allow(clippy::comparison_chain)]
+            if todos.len() == 1 {
+                heap.push(todos[0].clone());
+            } else if todos.len() > 1 {
+                let mut sub_paths = vec![];
+                for todo in todos {
+
+                    // Check if path from here
+                    let min_split = shortest_path_p1(grid, todo.position, goal, todo.dir);
+                    // println!("min split: {}: min: {}", todo.cost + min_split, start_cost + min);
+
+                    if todo.cost + min_split == start_cost + min {
+                        let sub_result = shortest_path_p2_recurse(
+                            grid,
+                            todo.position,
+                            todo.dir,
+                            todo.cost,
+                            goal,
+                            current_best_pos.clone(),
+                            seen.clone(),
+                        );
+
+                        if let Some(sub) = sub_result {
+                            sub_paths.push(sub.clone());
+                            if sub.0 == start_cost as i32 + min as i32 {
+                                results.extend(sub.1);
+                            }
+                        }
+                    }
+                }
+
+                // println!("save: {:?}", ((start), (start_dir), start_cost));
+                return Some((start_cost as i32 + min as i32, results));
+            }
         }
     }
 
-    // println!("could not reach goal from {start:?} with {init_dir:?}");
-    answers
+    None
 }
 
-fn shortest_path_p1(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) {
-    // dist[node] = current shortest distance from `start` to `node`
-    let dir = (1, 0);
+fn shortest_path_p2(
+    grid: &[Vec<char>],
+    start: (i32, i32),
+    start_dir: (i32, i32),
+    goal: (i32, i32),
+    min: usize,
+) {
+    let mut fifo = VecDeque::new();
+    let mut results = HashSet::new();
 
+    let dims = (grid[0].len() as i32, grid.len() as i32);
+
+    let initial_state = State {
+        cost: 0,
+        position: start,
+        prev: None,
+        dir: start_dir,
+    };
+
+    fifo.push_back(initial_state);
+
+    while let Some(state) = fifo.pop_front() {
+        let State {
+            cost,
+            position,
+            dir,
+            ref prev,
+        } = state;
+
+        if cost > min {
+            // already greater than the min path
+            continue;
+        }
+
+        let mut path: Vec<State> = vec![state.clone()];
+        let mut cur: Option<Box<State>> = state.prev.clone();
+
+        while let Some(prev) = cur {
+            path.push(*prev.clone());
+            cur = prev.prev;
+        }
+
+        let current_path: HashSet<(i32, i32)> =
+            HashSet::from_iter(path.iter().map(|state| state.position));
+        if current_path.len() < path.len() {
+            // That means that we passe two time on the same cell
+            continue;
+        }
+
+        let min_to_current = shortest_path_p1(grid, start, position, start_dir);
+        let min_to_end = shortest_path_p1(grid, position, goal, dir);
+
+        if cost > min_to_current + min_to_end {
+            // println!("cut min chelou");
+            continue;
+        }
+
+        if position == goal {
+            results.extend(path.iter().map(|state| state.position));
+        }
+
+        for vec in [dir, (dir.1, -dir.0), (-dir.1, dir.0)] {
+            let new_pos = (position.0 + vec.0, position.1 + vec.1);
+            if new_pos.0 >= 0 && new_pos.0 < dims.0 && new_pos.1 >= 0 && new_pos.1 < dims.1 {
+                let cell = grid[new_pos.1 as usize][new_pos.0 as usize];
+                if cell == '#' {
+                    continue;
+                } else {
+                    let score = if vec == dir {
+                        cost + 1
+                    } else {
+                        cost + 1000 + 1 //FIXME: not sure about this +1
+                    };
+
+                    let next = State {
+                        cost: score,
+                        position: new_pos,
+                        dir: vec,
+                        prev: Some(Box::new(state.clone())),
+                    };
+
+                    fifo.push_front(next);
+                }
+            }
+        }
+    }
+
+    println!("p2: {}", results.len());
+}
+
+fn shortest_path_p1(
+    grid: &[Vec<char>],
+    start: (i32, i32),
+    goal: (i32, i32),
+    dir: (i32, i32),
+) -> usize {
     let mut heap = BinaryHeap::new();
     let mut seen = HashSet::new();
 
@@ -270,6 +340,7 @@ fn shortest_path_p1(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) {
         cost: 0,
         position: start,
         dir,
+        prev: None,
     };
 
     heap.push(initial_state);
@@ -279,11 +350,11 @@ fn shortest_path_p1(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) {
             cost,
             position,
             dir,
+            prev,
         } = state;
 
         if position == goal {
-            println!("p1: {}", cost);
-            return;
+            return cost;
         }
 
         if seen.insert((position, dir)) {
@@ -304,6 +375,7 @@ fn shortest_path_p1(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) {
                             cost: score,
                             position: new_pos,
                             dir: vec,
+                            prev: None,
                         };
 
                         heap.push(next);
@@ -312,6 +384,7 @@ fn shortest_path_p1(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) {
             }
         }
     }
+    0
 }
 
 fn show_grid(grid: &[Vec<char>], paths: &HashSet<(i32, i32)>) {
